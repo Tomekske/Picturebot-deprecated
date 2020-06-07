02443eb31a09ef1c37751c86d6ff83f626105bb4
Tomek Joostens <joostenstomek@gmail.com>
2020-06-07 14:27:35 +0200

    First electron init + ASP .net core test - Render front-end - communicate between front-end and back-end

---
+++ PicturebotElectron/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ElectronNET.API;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PicturebotElectron.Models;
+
+namespace PicturebotElectron.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Electron.IpcMain.On("getData", (args) =>
+            {
+                var mainWindow = Electron.WindowManager.BrowserWindows.First();
+                Console.WriteLine("sendDataaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+                Electron.IpcMain.Send(mainWindow, "sendData", "Hello IPC World!");
+            });
+
+            
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
